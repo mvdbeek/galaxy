890eeb2affe2c71de0d308839b40a84f5bc10282
mvdbeek <m.vandenbeek@gmail.com>
2017-04-08 14:32:21 +0200

    Fix Instance is not bound to a Session
    by refreshing the tool_version_cache, just like we do when we reload the
    toolbox.

--- lib/galaxy/queue_worker.py
+++ lib/galaxy/queue_worker.py
@@ -122,12 +122,14 @@ def _get_new_toolbox(app):
 def reload_data_managers(app, **kwargs):
     start = time.time()
     from galaxy.tools.data_manager.manager import DataManagers
+    from galaxy.tools.toolbox.lineages.tool_shed import ToolVersionCache
     log.debug("Executing data managers reload on '%s'", app.config.server_name)
     app._configure_tool_data_tables(from_shed_config=False)
     reload_tool_data_tables(app)
     reload_count = app.data_managers._reload_count
     app.data_managers = DataManagers(app)
     app.data_managers._reload_count = reload_count + 1
+    app.tool_version_cache = ToolVersionCache(app)
     end = time.time() - start
     log.debug("Data Manager reload took %f seconds", end)
 
