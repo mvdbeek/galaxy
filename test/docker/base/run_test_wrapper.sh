01d2a9c3d09e3625189e6e55ab47f090b08b0904
mvdbeek <m.vandenbeek@gmail.com>
2019-01-02 15:48:41 +0100

    shell mught help?

--- test/docker/base/run_test_wrapper.sh
+++ test/docker/base/run_test_wrapper.sh
@@ -54,7 +54,7 @@ HOME=/home/galaxy
 find /usr/local/libexec/singularity -type f -name '*-suid' -exec chown root:root {} \; -exec chmod u+s {} \;
 
 echo "Testing singularity exec as galaxy user"
-sudo -E -u "#${GALAXY_TEST_UID}" singularity -d exec docker://busybox hostname
+sudo -E -u "#${GALAXY_TEST_UID}" sh -c "singularity -d exec docker://busybox hostname"
 
 echo "Running common startup for updated dependencies (if any)"
 sudo -E -u "#${GALAXY_TEST_UID}" ./scripts/common_startup.sh --dev-wheels || { echo "common_startup.sh failed"; exit 1; }
