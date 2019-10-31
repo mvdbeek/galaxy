1d425d2999b2c009e1f248eaeba51c157137228b
John Chilton <jmchilton@gmail.com>
2019-10-31 10:34:25 -0400

    Significant overhaul to container+dependency management
    *Overview*
    
    - Migrate "manage dependencies" UI to APIs and VueJS.
      - Add new `summarize_toolbox` API endpoint to mimic information produced by former grid.
      - The tool and requirement views are in the same grid now - depending on what you are viewing there is a "collapse tools" / "expand tools" button to swap to the other view.
      - Unused environments grid migrated as its own component.
      - By default information displayed information is more compact but every row can be expanded and many elements display a lot more information on mouse over.
    - Implement fallback container resolvers (describe below).
    - Implement RESTful API around container resolvers - describe both how resolvers are configured and the resolution of particular tools (or all tools). Add API endpoint for building containers as well.
    - Implement new VueJS table describing container resolution independent of dependencies. Allow mixed mode (show whatever container first resolved) as well as filtering by Docker/Singularity.
      - This information about containers is much more correct than what the manage dependencies grid would display - which was independent of tool IDs and based only on requirements, would not query quay.io for information, etc..
      - Since the grid is restricted to containers, embedded information is richer - displays information about the container resolver, a link to the container, correct table header, shell used by the container, etc..
    
    *Why Fallback Container Resolvers?*
    
    tl;dr - we can write better config files and more useful UIs if we specify default containers this way.
    
    Default containers can be specified in the job_conf but Galaxy doesn't really have the ability to dispatch or reason about such configuration until it is time to run the job. galaxy.tool_util has no clue about job configuration structure and job configuration structure can be (and often is) completely dynamic. As a result, such configuration should be thought of procedurally specified - configuration via container resolvers is effectively more explicitly annotated.
    
    This has two upshots for my current cloud dev push:
    
    - Containers specified this way can reasoned about in terms of the dependency magnagement API so we can provide an interface for seeing what tools will map to job (by default anyway - same stipulation applies to mulled containers which can also be overridden). I will utilize to make https://github.com/galaxyproject/galaxy/pull/8741.
    - While I think job_conf should ultimately allow overriding the container, I think the way this works out in most setups - including the CloudVE setup (https://github.com/galaxyproject/galaxy-helm/blob/master/galaxy/values.yaml#L201) the container is more realistically a global configuration setting. As such it is not naturally specified job_conf, awkardness results, and it would be much more cleanly specified in the container configuration - a global configuration.
    
    Handle empty requirements list in ResolutionDetails.vue.
    
    Error reported here https://github.com/galaxyproject/galaxy/pull/8741#issuecomment-550253702.

--- lib/galaxy/tool_util/deps/container_resolvers/explicit.py
+++ lib/galaxy/tool_util/deps/container_resolvers/explicit.py
@@ -4,6 +4,7 @@ import logging
 from ..container_resolvers import (
     ContainerResolver,
 )
+from ..requirements import ContainerDescription
 
 log = logging.getLogger(__name__)
 
@@ -24,6 +25,68 @@ class ExplicitContainerResolver(ContainerResolver):
                 return container_description
 
         return None
+
+
+class FallbackContainerResolver(ContainerResolver):
+    """Specify an explicit, identified container as a Docker container resolver."""
+
+    resolver_type = "fallback"
+    container_type = 'docker'
+
+    def __init__(self, app_info=None, shell="/bin/bash", identifier="", **kwds):
+        super(FallbackContainerResolver, self).__init__(app_info)
+        self.shell = shell
+        assert identifier, "fallback container resolver must be specified with non-empty identifier"
+        self.identifier = identifier
+
+    def _match(self, enabled_container_types, tool_info, container_description):
+        if self._container_type_enabled(container_description, enabled_container_types):
+            return True
+        return False
+
+    def resolve(self, enabled_container_types, tool_info, **kwds):
+        container_description = ContainerDescription(
+            self.identifier,
+            type=self.container_type,
+            shell=self.shell,
+        )
+        if self._match(enabled_container_types, tool_info, container_description):
+            return container_description
+
+
+class FallbackSingularityContainerResolver(FallbackContainerResolver):
+    """Specify an explicit, identified container as a Singularity container resolver."""
+
+    resolver_type = "fallback_singularity"
+    container_type = 'singularity'
+
+
+class FallbackNoRequirementsContainerResolver(FallbackContainerResolver):
+
+    resolver_type = "fallback_no_requirements"
+
+    def _match(self, enabled_container_types, tool_info, container_description):
+        type_matches = super(FallbackNoRequirementsContainerResolver, self)._match(enabled_container_types, tool_info, container_description)
+        return type_matches and (tool_info.requirements is None or len(tool_info.requirements) == 0)
+
+
+class FallbackNoRequirementsSingularityContainerResolver(FallbackNoRequirementsContainerResolver):
+
+    container_type = 'singularity'
+
+
+class RequiresGalaxyEnvironmentContainerResolver(FallbackContainerResolver):
+
+    resolver_type = "requires_galaxy_environment"
+
+    def _match(self, enabled_container_types, tool_info, container_description):
+        type_matches = super(RequiresGalaxyEnvironmentContainerResolver, self)._match(enabled_container_types, tool_info, container_description)
+        return type_matches and tool_info.requires_galaxy_python_environment
+
+
+class RequiresGalaxyEnvironmentSingularityContainerResolver(RequiresGalaxyEnvironmentContainerResolver):
+
+    container_type = 'singularity'
 
 
 class ExplicitSingularityContainerResolver(ExplicitContainerResolver):
@@ -50,4 +113,13 @@ class ExplicitSingularityContainerResolver(ExplicitContainerResolver):
         return None
 
 
-__all__ = ("ExplicitContainerResolver", "ExplicitSingularityContainerResolver")
+__all__ = (
+    "ExplicitContainerResolver",
+    "ExplicitSingularityContainerResolver",
+    "FallbackContainerResolver",
+    "FallbackSingularityContainerResolver",
+    "FallbackNoRequirementsContainerResolver",
+    "FallbackNoRequirementsSingularityContainerResolver",
+    "RequiresGalaxyEnvironmentContainerResolver",
+    "RequiresGalaxyEnvironmentSingularityContainerResolver",
+)
