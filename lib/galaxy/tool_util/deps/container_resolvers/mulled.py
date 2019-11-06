3ac70653cbf1cbdf5c813289e36c616cfbd02976
mvdbeek <m.vandenbeek@gmail.com>
2019-11-06 21:14:50 +0100

    Work around weird scoping issue
    For some reason `cached_name` didn't work even though Ijust
    added an entry to mulled_resolution_cache.
    Brings down the time for checking the container resolution
    from 90 seconds to 31.
    `

--- lib/galaxy/tool_util/deps/container_resolvers/mulled.py
+++ lib/galaxy/tool_util/deps/container_resolvers/mulled.py
@@ -250,21 +250,16 @@ def targets_to_mulled_name(targets, hash_func, namespace, resolution_cache=None)
 
     name = None
 
-    def cached_name(cache_key):
-        if mulled_resolution_cache:
-            if cache_key in mulled_resolution_cache:
-                return resolution_cache.get(cache_key)
-        return None
-
     if len(targets) == 1:
         target = targets[0]
         target_version = target.version
         cache_key = "ns[%s]__single__%s__@__%s" % (namespace, target.package_name, target_version)
         if cache_key in unresolved_cache:
             return None
-        name = cached_name(cache_key)
-        if name:
-            return name
+        try:
+            return mulled_resolution_cache.get(cache_key)
+        except Exception:
+            name = None
 
         tags = mulled_tags_for(namespace, target.package_name, resolution_cache=resolution_cache)
 
@@ -305,9 +300,10 @@ def targets_to_mulled_name(targets, hash_func, namespace, resolution_cache=None)
         cache_key = "ns[%s]__%s__%s" % (namespace, hash_func, base_image_name)
         if cache_key in unresolved_cache:
             return None
-        name = cached_name(cache_key)
-        if name:
-            return name
+        try:
+            return mulled_resolution_cache.get(cache_key)
+        except Exception:
+            name = None
 
         tag = first_tag_if_available(base_image_name)
         if tag:
