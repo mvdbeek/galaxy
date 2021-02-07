8816aa8bd476f04c7cef36739d13921c27ff4093
mvdbeek <m.vandenbeek@gmail.com>
2021-02-07 19:02:49 +0100

    Fix some more things

--- .ci/minikub-test-setup/start_services.sh
+++ .ci/minikub-test-setup/start_services.sh
@@ -4,9 +4,9 @@ set -ex
 kubectl create -f postgresConfig.yaml
 kubectl create -f statefulSet.yaml
 kubectl create -f service.yaml
-minikube service postgres-service --url=true
+minikube service testing-service --url=true
 
-GALAXY_TEST_DBURI="postgresql://postgres:postgres@localhost:$(kubectl get service postgres-service -o jsonpath='{.spec.ports[0].nodePort}')/galaxy?client_encoding=utf-8"
-GALAXY_TEST_AMQP_URL="amqp://localhost:$(kubectl get service postgres-service -o jsonpath='{.spec.ports[0].nodePort}')//"
+GALAXY_TEST_DBURI="postgresql://postgres:postgres@localhost:$(kubectl get service testing-service -o jsonpath='{.spec.ports[0].nodePort}')/galaxy?client_encoding=utf-8"
+GALAXY_TEST_AMQP_URL="amqp://localhost:$(kubectl get service testing-service -o jsonpath='{.spec.ports[1].nodePort}')//"
 export GALAXY_TEST_DBURI
 export GALAXY_TEST_AMQP_URL
