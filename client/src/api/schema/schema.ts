694130525f9dc6cc49ad0ae0f324f8538d159d39
mvdbeek <m.vandenbeek@gmail.com>
2021-11-09 10:00:10 +0100

    Add `fields` parameter to create collection schema
    Need to use an alias to not shadow the `BaseModel` attribute of the
    same name.

--- client/src/api/schema/schema.ts
+++ client/src/api/schema/schema.ts
@@ -2723,6 +2723,12 @@ export interface components {
              */
             element_identifiers?: components["schemas"]["CollectionElementIdentifier"][];
             /**
+             * Fields
+             * @description List of fields to create for this collection. Set to 'auto' to guess fields from identifiers.
+             * @default []
+             */
+            fields?: string | components["schemas"]["FieldDict"][];
+            /**
              * Folder Id
              * @description The ID of the library folder that will contain the collection. Required if `instance_type=library`.
              * @example 0123456789ABCDEF
@@ -2887,6 +2893,12 @@ export interface components {
              * @description List of elements that should be in the new collection.
              */
             element_identifiers?: components["schemas"]["CollectionElementIdentifier"][];
+            /**
+             * Fields
+             * @description List of fields to create for this collection. Set to 'auto' to guess fields from identifiers.
+             * @default []
+             */
+            fields?: string | components["schemas"]["FieldDict"][];
             /**
              * Folder Id
              * @description The ID of the library folder that will contain the collection. Required if `instance_type=library`.
@@ -4283,6 +4295,13 @@ export interface components {
                 | components["schemas"]["HdcaDataItemsFromTarget"]
                 | components["schemas"]["FtpImportTarget"]
             )[];
+        };
+        /** FieldDict */
+        FieldDict: {
+            /** Name */
+            name: string;
+            /** Type */
+            type: string;
         };
         /**
          * FileDataElement
