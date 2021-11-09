1f019a66ecef211398597ab710d10fa7e9996cd3
mvdbeek <m.vandenbeek@gmail.com>
2021-11-09 15:04:09 +0100

    Can't used TypedDict in python < 3.8

--- lib/galaxy/schema/schema.py
+++ lib/galaxy/schema/schema.py
@@ -9,7 +9,6 @@ from typing import (
     List,
     Optional,
     Tuple,
-    TypedDict,
     Union,
 )
 
@@ -965,11 +964,6 @@ class CollectionElementIdentifier(Model):
 CollectionElementIdentifier.update_forward_refs()
 
 
-class FieldDict(TypedDict):
-    name: str
-    type: str
-
-
 class CreateNewCollectionPayload(Model):
     collection_type: CollectionType = CollectionTypeField
     element_identifiers: List[CollectionElementIdentifier] = Field(
@@ -1005,7 +999,7 @@ class CreateNewCollectionPayload(Model):
         default=None,
         description="The ID of the history that will contain the collection. Required if `instance_type=library`.",
     )
-    fields: Union[str, Optional[List[FieldDict]]] = Field(
+    fields: Optional[Union[str, List[Dict['str', 'str']]]] = Field(
         default=[],
         description="List of fields to create for this collection. Set to 'auto' to guess fields from identifiers."
     )
