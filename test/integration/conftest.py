baa4e6aa9e13afdc65032f88e2392e40d9cbd4f4
mvdbeek <m.vandenbeek@gmail.com>
2022-03-30 09:54:41 +0200

    Fix pytest_configure import

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -2,7 +2,7 @@ import tempfile
 
 import pytest
 
-from galaxy_test.api.conftest import pytest_configure  # noqa: F401
+from galaxy_test.conftest import pytest_configure  # noqa: F401
 
 
 def pytest_collection_finish(session):
