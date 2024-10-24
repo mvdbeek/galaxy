e41e3cb2f87215abc5d7d07142d01e78c6599f50
mvdbeek <m.vandenbeek@gmail.com>
2024-10-24 08:05:04 -0400

    Baisc boxplot

--- client/src/components/WorkflowInvocationState/WorkflowInvocationMetrics.vue
+++ client/src/components/WorkflowInvocationState/WorkflowInvocationMetrics.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { BCard } from "bootstrap-vue";
-import { GalaxyApi, type components } from "@/api";
 import { computed, ref, watch } from "vue";
-import type JobMetrics from "../JobMetrics/JobMetrics.vue";
+
+import { type components, GalaxyApi } from "@/api";
 import { errorMessageAsString } from "@/utils/simple-error";
-import MetricsBoxPlot from "./MetricsBoxPlots.vue";
+
+import VegaWrapper from "./VegaWrapper.vue";
 
 const props = defineProps({
     invocationId: {
@@ -13,40 +13,79 @@ const props = defineProps({
     },
 });
 
-
-const jobMetrics = ref<components["schemas"]["JobMetric"][]>()
-const fetchError = ref<string>()
+const jobMetrics = ref<components["schemas"]["WorkflowJobMetric"][]>();
+const fetchError = ref<string>();
 
 async function fetchMetrics() {
-    const {data, error} = await GalaxyApi().GET("/api/invocations/{invocation_id}/metrics", {
+    const { data, error } = await GalaxyApi().GET("/api/invocations/{invocation_id}/metrics", {
         params: {
             path: {
-                invocation_id: props.invocationId
-            }
-        }
-    })
+                invocation_id: props.invocationId,
+            },
+        },
+    });
     console.log("data", data, error);
     if (error) {
-        fetchError.value = errorMessageAsString(error)
+        fetchError.value = errorMessageAsString(error);
     } else {
-        jobMetrics.value = data
+        jobMetrics.value = data;
     }
 }
 
-
-watch((props), () => fetchMetrics(), { immediate: true })
+watch(props, () => fetchMetrics(), { immediate: true });
 
 const wallclock = computed(() => {
-    return jobMetrics.value?.filter((jobMetric) => jobMetric.name == "runtime_seconds")
-})
+    const wallclock = jobMetrics.value?.filter((jobMetric) => jobMetric.name == "runtime_seconds");
+    return wallclock?.map((item) => {
+        return {
+            raw_value: parseFloat(item.raw_value),
+            tool_id: item.tool_id,
+        };
+    });
+});
 
+const spec = computed(() => {
+    console.log(wallclock.value);
+    return {
+        $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+        description: "A boxplot with jittered points.",
+        data: {
+            values: wallclock.value,
+        },
+        layer: [
+            {
+                mark: { type: "boxplot", opacity: 0.5 },
+                encoding: {
+                    x: { field: "tool_id", type: "ordinal" },
+                    y: { field: "raw_value", type: "quantitative" },
+                },
+                width: "container",
+            },
+            {
+                mark: {
+                    type: "point",
+                    opacity: 0.7,
+                },
+                encoding: {
+                    x: {
+                        field: "tool_id",
+                        type: "ordinal",
+                        bandPosition: { signal: "(random() + 0.05)" }, // Apply random jitter within the categorical band
+                    },
+                    y: {
+                        field: "raw_value",
+                        type: "quantitative",
+                        axis: { title: "Raw Value" },
+                        scale: { zero: false },
+                    },
+                },
+                width: "container",
+            },
+        ],
+    };
+});
 </script>
 
 <template>
-    <MetricsBoxPlot v-if="wallclock" :job-data="wallclock"/>
-    <!--
-    <div>
-        {{ jobMetrics }}
-    </div>  
-    -->
+    <VegaWrapper v-if="spec" :spec="spec" />
 </template>
