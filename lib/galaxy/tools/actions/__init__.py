3b91dff6c9695701d4927a078122965558bf6e94
mvdbeek <m.vandenbeek@gmail.com>
2025-01-10 16:06:36 +0200

    Simplify determine_output_format

--- lib/galaxy/tools/actions/__init__.py
+++ lib/galaxy/tools/actions/__init__.py
@@ -3,6 +3,7 @@ import logging
 import os
 import re
 from abc import abstractmethod
+from collections.abc import MutableMapping
 from json import dumps
 from typing import (
     Any,
@@ -507,7 +508,7 @@ class DefaultToolAction(ToolAction):
                     output,
                     wrapped_params.params,
                     inp_data,
-                    inp_dataset_collections,
+                    input_collections,
                     input_ext,
                     python_template_version=tool.python_template_version,
                     execution_cache=execution_cache,
@@ -1164,7 +1165,7 @@ def determine_output_format(
     output: "ToolOutput",
     parameter_context,
     input_datasets,
-    input_dataset_collections,
+    input_dataset_collections: MutableMapping[str, model.HistoryDatasetCollectionAssociation],
     random_input_ext,
     python_template_version="3",
     execution_cache=None,
@@ -1206,7 +1207,7 @@ def determine_output_format(
 
         if collection_name in input_dataset_collections:
             try:
-                input_collection = input_dataset_collections[collection_name][0][0]
+                input_collection = input_dataset_collections[collection_name]
                 input_collection_collection = input_collection.collection
                 if element_index is None:
                     # just pick the first HDA
